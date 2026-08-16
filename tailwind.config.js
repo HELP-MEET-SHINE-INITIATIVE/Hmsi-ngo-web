5c173c754ff317ec5443be35038bce8eef175782
Gpowerluv <Gpowerluv@users.noreply.github.com>
2026-08-16 03:01:15 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
+export default config;
